f9149e3102111331ed264103eec1b04207b0b421
firozsaifi24 <firozsaifi24@gmail.com>
2019-05-23 15:01:45 +0530

    This is also committed by firoz

---
+++ app/src/main/java/example/firoz/gitversioncontrolsystemvcs/DataRepository.java
@@ -0,0 +1,15 @@
+package example.firoz.gitversioncontrolsystemvcs;
+
+public class DataRepository {
+
+    public String dataString()
+    {
+        return "This is my repo";
+    }
+
+    public String dataString2()
+    {
+        return "This is your repo";
+    }
+
+}
